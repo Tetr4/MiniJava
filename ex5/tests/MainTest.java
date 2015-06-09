96174845f399e78636262dddc0074b3697958b83
Mike <m.klimek@tu-bs.de>
2015-06-09 20:33:10 +0200

    more tests

--- ex5/tests/MainTest.java
+++ ex5/tests/MainTest.java
@@ -1,15 +1,32 @@
 class MainTest{
-    public static void main(String[] a){
+    public static void main(String[] abc){ // bla
     	MAMA ma;
+    	/*asdf*/
+    	moep = asdf.length;
+    	moep = moep;
+    	
     	ma = new MAMA();
     	hoden = ma.bla(x, y);
 	System.out.println(false);
     }
 }
 
-class MAMA {
+class MAMA extends MainTest {
 	
-	public MainTest bla(int[] x, int yo) {
+	public MainTest bla(int[] x, int yo, boolean lol, asdf wat) {
+	    int a;
+	    
+	    if ( a < a ) {
+	        System.out.println( (((true))) );
+	    } else {}
+	        
+	    while(true) 
+	        System.out.println(lol);
+	    
+	    a = b;
+	    a[b] = c;
+	    a = !3 && 4*3+4 - new Blubb();
+	    asdf[3] = 4*3;
 		System.out.println(x.length);
 		return 123;
 	}
