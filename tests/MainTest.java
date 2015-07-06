859edec3a7f29fe3ee62d06f875552cf146bc9dc
Mike <m.klimek@tu-bs.de>
2015-07-06 23:57:57 +0200

    more grammar tests

--- tests/MainTest.java
+++ tests/MainTest.java
@@ -1,33 +1,46 @@
-class MainTest{
-    public static void main(String[] abc){ // bla
-    	MAMA ma;
-    	/*asdf*/
-    	moep = asdf.length;
-    	moep = moep;
+class Bla {
+    public static void main(String[] bla){ // bla
+        // VarDecl*
+    	int[] a; /* muh */
+    	boolean b;
+    	int c;
+    	Bla d;
     	
-    	ma = new MAMA();
-    	//hoden = ma.bla(x, y);
-	System.out.println(false);
+    	// Statement*
+    	a = new int[1];
+    	b = true;
+    	c = 1;
+    	d = new Blubb();
     }
 }
 
-class MAMA extends MainTest {
+class Blubb extends Bla {
+    /*a*/
+    // VarDecl*
+    Blubb blubb;
 	
-	public MainTest bla(int[] x, int yo, boolean lol, asdf wat) {
-	    int a;
+    // Method*
+	public Bla meh(int[] a, boolean b, int c, Bla d) {
+	    // VarDecl*	    
+	    int x;
+	    // Statement*
+	    {}
+	    { System.out.println(1); }
+	    while(false)
+	        x=5+x;
+	    //if ( a.length < x ) {
+	    if ( a < x ) {
+	        a[0] = 5;
+	    } else {
+	        a = new int[5];
+	        b = !b;
+	        d = new Blubb();
+	        d = this.meh(a, b, c, d);
+	        b = c+1-1*1 < c+1 && c < 10;
+	    }
+	    blubb = new Blubb();
 	    
-	    if ( a < a ) {
-	        System.out.println( (((true))) );
-	    } else {}
-	        
-	    while(true) 
-	        System.out.println(lol);
-	    
-	    a = b;
-	    a[b] = c;
-	    a = !3 && 4*3+4 - new Blubb();
-	    asdf[3] = 4*3;
-		System.out.println(x.length);
-		return 123;
+	    // Return
+		return blubb;
 	}
 }
