7c4c7b8668ccbd15535d3c89d3ae6f732e710bb6
Mike <m.klimek@tu-bs.de>
2015-07-12 16:54:32 +0200

    Add class for running all tests at once

---
+++ src/test/AllTests.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ScannerTest.class,ParserTest.class,NameResolutionTest.class,ErrorCheckTest.class})
+public class AllTests {
+}
