6449949468520935935ea0d7d3804861d808b5a5
Tetr4 <Tetr4@users.noreply.github.com>
2015-08-12 21:27:42 +0200

    Prettyprint spiglet in MJFrontEnd - use dummy values for temps - add static interpret function

--- src/minijava/MJFrontEnd.java
+++ src/minijava/MJFrontEnd.java
@@ -2,10 +2,12 @@
 package minijava;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Collection;
@@ -13,7 +15,7 @@ import java.util.Collection;
 import syntaxtree.Node;
 import visitor.GJPigletInterpreter;
 
-class MJFrontEnd {
+public class MJFrontEnd {
 
     public static void main(String args[]) {
         String inputFileName;
@@ -31,9 +33,11 @@ class MJFrontEnd {
             minijava.Program program = (Program) parser.parse(scanner);
 
             // Print the resulting AST on standard output.
+            System.out.println("### MiniJava - AST ###");
             System.out.println(program.printAST());
 
             // Prettyprint the program
+            System.out.println("### MiniJava - Pretty Print ###");
             System.out.println(program.print().getString());
             
             // Print errors
@@ -50,27 +54,22 @@ class MJFrontEnd {
             piglet.Program piglet = program.toPiglet();
             
             // Prettyprint piglet
+            System.out.println("### Piglet - Pretty Print ###");
             String pigletCode = piglet.print().getString();
             System.out.println(pigletCode);
             
-            System.out.println("Interpreting piglet program:");
             // Interpret piglet program
-            try {
-                // You can't import types from default package :/
-                // Screw pgi.jar >:(
-                Class<?> pigletInterpreterClass = Class.forName("PigletParser");
-                Constructor<?> pigletInterpreterConstructor = pigletInterpreterClass.getConstructor(InputStream.class);
-                Method pigletInterpreterGoalMethod = pigletInterpreterClass.getMethod("Goal");
-                
-                // Create piglet code input reader from pretty printer output
-                InputStream pigletInput = new ByteArrayInputStream(pigletCode.getBytes());
-                
-                Node root = (Node) pigletInterpreterGoalMethod.invoke(pigletInterpreterConstructor.newInstance(pigletInput));
-                root.accept(new GJPigletInterpreter("MAIN", null, root), root);
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            System.out.println("### Piglet - Interpreting ###");
+            ByteArrayOutputStream stream = interpret(pigletCode);
+            System.out.println(stream.toString());
+            
+            // Transform piglet AST to spiglet AST
+            spiglet.Program spiglet = piglet.toSpiglet();
+            
+            // Prettyprint spiglet
+            System.out.println("### Spiglet - Pretty Print ###");
+            String spigletCode = spiglet.print().getString();
+            System.out.println(spigletCode);
 
         } catch (FileNotFoundException e) {
             System.err.println("MJFrontEnd: file " + inputFileName + " not found");
@@ -79,7 +78,40 @@ class MJFrontEnd {
             System.out.println(e.getMessage());
         } catch (IOException e) {
             System.out.println("MJFrontEnd: " + e.getMessage());
+        } catch (ReflectiveOperationException e) {
+            System.out.println("Error setting up interpreter:");
+            e.printStackTrace();
         }
+    }
+    
+    public static ByteArrayOutputStream interpret(String pigletCode)
+            throws IOException, ReflectiveOperationException {
+        // You can't import types from default package :/
+        // Screw pgi.jar >:(
+        Class<?> interpreterClass = Class.forName("PigletParser");
+        Constructor<?> interpreterConstructor = interpreterClass.getConstructor(InputStream.class);
+        Method interpreterGoalMethod = interpreterClass.getMethod("Goal");
+        
+        // Create piglet code input reader from pretty printer output
+        InputStream pigletInput = new ByteArrayInputStream(pigletCode.getBytes());
+
+        // redirect global System.out so we can compare interpreter output...
+        PrintStream savedStream = System.out;
+        savedStream.flush();
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outStream);
+        System.setOut(printStream);
+
+        
+        // interpret -> prints to redirected System.out
+        Node root = (Node) interpreterGoalMethod.invoke(interpreterConstructor.newInstance(pigletInput));
+        root.accept(new GJPigletInterpreter("MAIN", null, root), root);
+
+        // undo System.out redirect
+        System.setOut(savedStream);
+        
+
+        return outStream;
     }
 
 }
