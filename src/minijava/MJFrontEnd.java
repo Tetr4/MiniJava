aa93410f3b5a3c2289e89677268074cfbab18254
Peace-Maker <peace-maker@wcfan.de>
2015-10-10 01:58:43 +0200

    Add -interpret and -liveness command line options
    Optionally interpret the mips code or perform liveness analysis
    depending on given command line arguments in the frontend. Using apache
    commons cli.

--- src/minijava/MJFrontEnd.java
+++ src/minijava/MJFrontEnd.java
@@ -6,18 +6,39 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Collection;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class MJFrontEnd {
     private static final int ERROR = 1;
 
     public static void main(String args[]) {
+        Options options = new Options();
+        options.addOption("liveness", false, "perform liveness analysis on temps during SPiglet phase");
+        options.addOption("interpret", false, "interpret program using SPIM");
+        
+        CommandLineParser cmdParser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = cmdParser.parse(options, args);
+        } catch (ParseException e1) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("MJFrontEnd <options> file", options);
+            System.exit(ERROR);
+        }
+        
         // choose input source
         MJScanner scanner = null;
-        if (args.length == 0) {
+        if (cmd.getArgs().length == 0) {
             // pipe or interactive shell
             scanner = new MJScanner(System.in);
-        } else if (args.length == 1) {
+        } else if (cmd.getArgs().length == 1) {
             // read from file
-            String inputFileName = args[0];
+            String inputFileName = cmd.getArgs()[0];
             try {
                 scanner = new MJScanner(new FileReader(inputFileName));
             } catch (FileNotFoundException e) {
@@ -51,11 +72,22 @@ public class MJFrontEnd {
             System.exit(ERROR);
         }
 
-        // Transform minijava AST to mips AST
-        mips.Program mips = program.toPiglet().toSpiglet().liveness().toKanga().toMips();
+        // Transform minijava AST to spiglet AST
+        spiglet.Program spiglet = program.toPiglet().toSpiglet();
+        // Perform optional liveness analysis
+        if (cmd.hasOption("liveness")) {
+            spiglet.liveness();
+        }
+        // Transform spiglet AST to mips AST
+        mips.Program mips = spiglet.toKanga().toMips();
 
         // Print mips code
         System.out.println(mips.print().getString() + '\n');
+        
+        if (cmd.hasOption("interpret")) {
+            System.out.println("Interpreting MIPS code...");
+            System.out.println(mips.interpret());
+        }
     }
 
 }
